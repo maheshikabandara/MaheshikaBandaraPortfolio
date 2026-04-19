d97a7566c121419e2bb88cb0418ccf7757dfa9eb
Maheshika Bandara <159937346+maheshikabandara@users.noreply.github.com>
2026-04-19 17:10:23 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        albert: ['"Albert Sans"', 'sans-serif'],
+        instrument: ['"Instrument Serif"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
